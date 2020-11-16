3838ee367c0628aa761b728233070eb80a12dafa
Emanuel Teodor Blaga <manub7@yahoo.com>
2020-11-16 22:24:47 +0000

    Working on getVenueInfomation to render places and fixings bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,74 @@
-    
-    
-   /* function showVenues(venues){
 
-        var venues = venues;
-        console.log(venues);
-       /* $("venue1").html(showVenuesHTML(venue1));
-      
-        /*  var venuesArray = [$('venue1'),$('venue2'),$('venue3')];
-        
-        venuesArray.forEach((venue,index) => {
-            venues.forEach((element, index) => {
-            var venue = element[index];
-            showVenuesHTML(venue);
-        });
-
-        /*venues.forEach((element, index) => {
-            var venue = element[index];
-            
-        })}*/
-    
-    /*function showVenuesHTML(venue){
+    function showVenuesHTML(venue){
        return `<h2>${venue.name}</h2>
-                <img class="venueimage" src="${iconSource}"/>
                 <h3>Address:</h3>
                 <p>${venue.location.address}</p>
                 <p>${venue.location.city}</p>
-                <p>${venue.location.country}</p>`;
-    }*/
+                <p>${venue.location.country}</p>`
+    }
 
     function getVenueInformation(cityName) {
 
-        var clientId = 'PU3IY1PZEOOANTPSHKNMS5HFSMEGEQ1IAVJYGYM4YVZP3NGD';
-        var clientSecret = '0V21IXU0EETE3SZJGGCP4T4R13NUTBJ0LMI5WQY45IMDPEKY';
+        var clientId ='PU3IY1PZEOOANTPSHKNMS5HFSMEGEQ1IAVJYGYM4YVZP3NGD';
+        var clientSecret ='0V21IXU0EETE3SZJGGCP4T4R13NUTBJ0LMI5WQY45IMDPEKY';
         var cityName = cityName;
          
             $.when(
             
-                $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=${clientId}&client_secret=${clientSecret}&v=20201112&near=${cityName}&limit=3`)
+                $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=${clientId}&client_secret=${clientSecret}&v=20191112&near=${cityName}&limit=3`)
             
-                ).then(function(data){
-                    var venues = data;
+                ).then( function(data){
+                    var venues = data.response.venues;
                     console.log(venues);
-                    
-                })
+                   /*Get data for each venue index and display it on the venues divs*/ 
+                    var venue1 = venues[0];
+                    console.log(venue1);
+                    var venue2 = venues[1];
+                    var venue3 = venues[2];
+                     $("#venue1").html(showVenuesHTML(venue1));
+                     $("#venue2").html(showVenuesHTML(venue2));
+                     $("#venue3").html(showVenuesHTML(venue3));
 
-    }
+                      }, function(errorResp) {
+            if (errorResp.status === 404) {
+                $("#destiantionCity").html(
+                    `<h2>No info found for user ${cityName}</h2>`);
+            
+            }else {
+                
+                $("#destiantionCity").html(
+                    `<h2>Error: ${errorResp.responseJSON.message}</h2>`);
+            }
+                      
+      })
+}
 
-
+/* event handler */
     $(document).ready(function(){
     // Get value on button click and show alert
     $("#test").click(function() {
+        /* $("#destiantionCity").html("");
+         $("#venue1").html(showVenuesHTML(""));
+         $("#venue2").html(showVenuesHTML(""));
+         $("#venue3").html(showVenuesHTML(""));*/
+
+        /*Getting the "#city" value and checking if the value exists*/
         var cityName = $("#city").val();
-        alert(cityName);
-        console.log(cityName); 
-        getVenueInformation(cityName);
-        /*showVenues(items);*/
-    })
-})
+      /*  if(!cityName) {
+                $("#destiantionCity").html(`<h2>Please input city</h2>`);
+                return;
+        } /*else {
+
+         $("#destiantionCity").html(`<h2>${cityName}</h2>`);
+
+             alert(cityName);
+             getVenueInformation(cityName);
+               
+            
+                     }*/
+            $("#destiantionCity").html(`<h2>${cityName}</h2>`);
+
+             alert(cityName);
+             getVenueInformation(cityName);
+                })
+        })
