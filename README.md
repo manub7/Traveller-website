112bc37d2011c4bd2c14c00dd4c2df9d860412a2
Emanuel Teodor Blaga <manub7@yahoo.com>
2020-11-18 22:01:03 +0000

    Fixed the bugs, added weather getJson, set up show weather info for multiple days forcast and added some styling

---
+++ README.md
@@ -0,0 +1,87 @@
+# Traveller website
+
+    A site destined for travelers wanting to find instant information about a particular location, information will include top 3 venues search and weather
+ 
+## UX
+ 
+Use this section to provide insight into your UX process, focusing on who this website is for, what it is that they want to achieve and how your project is the best way to help them achieve these things.
+
+In particular, as part of this section we recommend that you provide a list of User Stories, with the following general structure:
+- As a user type, I want to perform an action, so that I can achieve a goal.
+
+This section is also where you would share links to any wireframes, mockups, diagrams etc. that you created as part of the design process. These files should themselves either be included as a pdf file in the project itself (in an separate directory), or just hosted elsewhere online and can be in any format that is viewable inside the browser.
+
+## Features
+
+In this section, you should go over the different parts of your project, and describe each in a sentence or so.
+ 
+### Existing Features
+- Feature 1 - allows users X to achieve Y, by having them fill out Z
+- ...
+
+For some/all of your features, you may choose to reference the specific project files that implement them, although this is entirely optional.
+
+In addition, you may also use this section to discuss plans for additional features to be implemented in the future:
+
+### Features Left to Implement
+- Another feature idea
+
+## Technologies Used
+
+In this section, you should mention all of the languages, frameworks, libraries, and any other tools that you have used to construct this project. For each, provide its name, a link to its official site and a short sentence of why it was used.
+
+- [JQuery](https://jquery.com)
+    - The project uses **JQuery** to simplify DOM manipulation.
+
+
+## Testing
+
+In this section, you need to convince the assessor that you have conducted enough testing to legitimately believe that the site works well. Essentially, in this part you will want to go over all of your user stories from the UX section and ensure that they all work as intended, with the project providing an easy and straightforward way for the users to achieve their goals.
+
+Whenever it is feasible, prefer to automate your tests, and if you've done so, provide a brief explanation of your approach, link to the test file(s) and explain how to run them.
+
+For any scenarios that have not been automated, test the user stories manually and provide as much detail as is relevant. A particularly useful form for describing your testing process is via scenarios, such as:
+
+1. Contact form:
+    1. Go to the "Contact Us" page
+    2. Try to submit the empty form and verify that an error message about the required fields appears
+    3. Try to submit the form with an invalid email address and verify that a relevant error message appears
+    4. Try to submit the form with all inputs valid and verify that a success message appears.
+
+In addition, you should mention in this section how your project looks and works on different browsers and screen sizes.
+
+You should also mention in this section any interesting bugs or problems you discovered during your testing, even if you haven't addressed them yet.
+
+If this section grows too long, you may want to split it off into a separate file and link to it from here.
+
+
+
+I have spent a lot of time trying to fix the event handler bubbling issue, the event handler was launching and displaying briefly the API information only to dispear in a few milisecconds.
+I spend some time researching this issue and found it to be relatate to the syntheticEvent propagating itself in the bubble. 
+The  documentation that helped me fix the issue can be found [here](https://reactjs.org/docs/events.html#mouse-events) and [here](https://stackoverflow.com/questions/4384829/jquery-onclick-capture-the-id-of-the-element/39629863)
+
+## Deployment
+
+This section should describe the process you went through to deploy the project to a hosting platform (e.g. GitHub Pages or Heroku).
+
+In particular, you should provide all details of the differences between the deployed version and the development version, if any, including:
+- Different values for environment variables (Heroku Config Vars)?
+- Different configuration files?
+- Separate git branch?
+
+In addition, if it is not obvious, you should also describe how to run your code locally.
+
+
+## Credits
+
+### Content
+- The text for section Y was copied from the [Wikipedia article Z](https://en.wikipedia.org/wiki/Z)
+
+### Media
+- The photos used in this site were obtained from ...
+
+### Acknowledgements
+
+- I received inspiration for this project from X
+
+Current data from [here].(https://stackoverflow.com/questions/8398897/how-to-get-current-date-in-jquery)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,169 @@
 
-    function showVenuesHTML(venue){
+   function showVenuesHTML(venue){
        return `<h2>${venue.name}</h2>
                 <h3>Address:</h3>
                 <p>${venue.location.address}</p>
                 <p>${venue.location.city}</p>
                 <p>${venue.location.country}</p>`
-    }
+    };
 
-    function getVenueInformation(cityName) {
+    function showWeatherHTML(venueSet1,venueSet2) {
 
+            var apiName = venueSet2.city.name;
+            console.log(apiName)
+            $("#apiName").html(`<h2>${apiName}</h2>`)
+            var venueList = venueSet1.daily;
+            console.log(venueList)
+            /*var day = d.getDate()+ "/" + (d.getMonth()+1) + "/"+d.getFullYear();*/
+            venueList.forEach( function(date){
+                var temp = date.temp.day.toFixed(0);
+                var cond = date.weather[0].main;
+                var d = date.dt*1000;
+                console.log(d);
+                day = new Date(d);
+                var dayString = day.toLocaleDateString("en-GB",{year:"2-digit",month:"2-digit", day:"2-digit"});
+                console.log(dayString)
+                console.log(temp)
+                console.log(cond)
+                $("#weatherData").append(` <div>
+                                          <img src="https://openweathermap.org/img/wn/${date.weather[0].icon}@2x.png"/>
+                                          <h2>${temp}°C</h2>
+                                          <h2>${cond}</h2>
+                                          <h2>${dayString}</h2>
+                                          </div>`)
+            })
+    
+        }
+
+/* Get venue infomation function*/
+    function getVenueInformation(city) {
+
+        /* Declaring  the variables necessary to access the API*/
         var clientId ='PU3IY1PZEOOANTPSHKNMS5HFSMEGEQ1IAVJYGYM4YVZP3NGD';
         var clientSecret ='0V21IXU0EETE3SZJGGCP4T4R13NUTBJ0LMI5WQY45IMDPEKY';
-        var cityName = cityName;
-         
+        var cityName=city;
+                 
             $.when(
             
                 $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=${clientId}&client_secret=${clientSecret}&v=20191112&near=${cityName}&limit=3`)
             
-                ).then( function(data){
-                    var venues = data.response.venues;
-                    console.log(venues);
-                   /*Get data for each venue index and display it on the venues divs*/ 
+                ).then (function(data){
+                   
+                     
+                     /* Access specific data from the object*/
+                     var venues = data.response.venues;
+                    /*console.log(venues);*/
+                   
+                    /*Get data for each venue index and display it on the venues divs*/ 
+                    
                     var venue1 = venues[0];
-                    console.log(venue1);
+                   /* console.log(venue1); */
                     var venue2 = venues[1];
                     var venue3 = venues[2];
+
+                    /* Call showVenuesHTML to display data in the div*/
                      $("#venue1").html(showVenuesHTML(venue1));
                      $("#venue2").html(showVenuesHTML(venue2));
                      $("#venue3").html(showVenuesHTML(venue3));
 
-                      }, function(errorResp) {
-            if (errorResp.status === 404) {
-                $("#destiantionCity").html(
-                    `<h2>No info found for user ${cityName}</h2>`);
+                      } , function(errorResp) {
+            if (cityName === "") {
+                $("#messageBox").html(
+                    `<p>Please input destination</p>`);
             
-            }else {
+            }else if(errorResp.status === 400) {
                 
-                $("#destiantionCity").html(
-                    `<h2>Error: ${errorResp.responseJSON.message}</h2>`);
+                $("#messageBox").html(
+                    `<p>No information found for "${cityName}", try again </p>`);
+                   
+            }
+            else {
+                
+                $("#messageBox").html(
+                    `<p>Error: ${errorResp.responseJSON.message}</p>`);
+            }
+                      
+      }
+    )
+};
+
+/* Get Weather information function */
+
+    function getWeatherInfomration(city) {
+        /* Declaring  the variables necessary to access the API*/
+        var apiKey = "bd582b7a8caf8259d410f2f2bd53bf07"
+        var cityName = city;
+      
+        $.when(
+            
+                $.getJSON(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&cnt=5&appid=${apiKey}` )
+                            
+                ).then (function(data){
+
+                  /* Access specific data from the object*/
+                     var weatherInfo = data;
+                     var coord =  data.city.coord
+                     var lat = coord.lat;
+                     var lon = coord.lon;
+                     console.log(coord, lat, lon)
+                     
+                     
+
+                    $.when(
+            
+                             $.getJSON(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}` ).
+                             
+                             then (function(data){
+                                var newWeather = data;
+                                var dailyData = newWeather.daily;
+                                console.log(newWeather)
+                                showWeatherHTML(newWeather,weatherInfo);
+
+                             }))} , function(errorResp) {
+            if (cityName === "") {
+                $("#messageBox").html(
+                    `<p>Please input destination</p>`);
+            
+            }else if(errorResp.status === 400) {
+                
+                $("#messageBox").html(
+                    `<p>No information found for "${cityName}", try again </p>`);
+                   
+            }
+            else {
+                
+                $("#messageBox").html(
+                    `<p>Error: ${errorResp.responseJSON.message}</p>`);
             }
                       
       })
-}
+    }
 
-/* event handler */
-    $(document).ready(function(){
-    // Get value on button click and show alert
-    $("#test").click(function() {
-        /* $("#destiantionCity").html("");
-         $("#venue1").html(showVenuesHTML(""));
-         $("#venue2").html(showVenuesHTML(""));
-         $("#venue3").html(showVenuesHTML(""));*/
 
-        /*Getting the "#city" value and checking if the value exists*/
-        var cityName = $("#city").val();
-      /*  if(!cityName) {
-                $("#destiantionCity").html(`<h2>Please input city</h2>`);
-                return;
-        } /*else {
+   /* event handler*/
+   $(document).ready(function() {
 
-         $("#destiantionCity").html(`<h2>${cityName}</h2>`);
-
-             alert(cityName);
-             getVenueInformation(cityName);
-               
-            
-                     }*/
-            $("#destiantionCity").html(`<h2>${cityName}</h2>`);
-
-             alert(cityName);
-             getVenueInformation(cityName);
-                })
-        })
+      $("#buttonS").on("click", function(e) {
+       
+        /* Get the text box value */
+         e.preventDefault()
+         /* Clear divs*/
+         $("#weatherData").empty();
+         $("#venue1").html("");
+         $("#venue2").html("");
+         $("#venue3").html("");
+        /* Getting the input value to assign it to function*/
+         var cityName = $("#city").val();
+         alert(cityName);
+            if (cityName==="") {
+                 $("#messageBox").html(`<p>No input, please input destination</p>`);
+                 $("#apiDiv").css("visibility","hiden");
+                   
+            }else {
+                 /*Calling the fetch API infomation with the argument value of cityName */
+                    $("#apiDiv").css("visibility","visible");
+                    getVenueInformation(cityName);
+                    getWeatherInfomration(cityName);
+                }
+          })
+    })
